57ae720d7ccee479daefbb2281fa29ad88acf5d7
JonHaase <jonjhaase@gmail.com>
2020-06-28 11:30:19 -0400

    updating soundfile

--- soundfile.py
+++ soundfile.py
@@ -1,20 +1,24 @@
-from pydub import AudioSegment
-from pydub.playback import play
 import numpy as np
 import simpleaudio as sa
+import os
+from pydub import AudioSegment
+from pydub.playback import play
+
 from scipy.io.wavfile import read
 
+source_file = "/home/jon//Documents/birdsong//home/jon//Documents/birdsong/cat.wav"  # here is the audio fil
+
 #  using PyDub  and ffmpeg to slice an audio file and convert MP3 to WAV
-song = AudioSegment.from_mp3("XC2628.mp3")
+song = AudioSegment.from_mp3(source_file)
 aud_start_num = 1000 * 1  # 1000 * lenght of audio in seconds
-aud_end_num = 1000 * .3
+aud_end_num = 1000 * .3 
 aud_start = song[:aud_start_num]
 aud_end = song[-aud_end_num:]
-aud_start.export("XC2628_10Sec.wav", format="wav")
+aud_start.export(source_file_export, format="wav")
 play(aud_start)  # slide five seconds of start of audio
 
 #  using scipy to create a numpy array from audio
-a = read("XC2628_10Sec.wav")
+a = read(bird_sound)
 print("tuple")
 print(a)
 print(type(a))
@@ -22,5 +26,7 @@ a = np.array(a[1])
 print("np array")
 print(a)
 print(type(a))
-ffmpeg -i /home/jon//Documents/birdsong/C2628_10Sec.wav -filter_complex stereotools=phasel=1 -ac 1 /home/jon//Documents/birdsong/output.wav
+os.system (ffmpeg -i /home/jon//Documents/birdsong/cat.wav -map_channel 0.0.0 left.wav -map_channel 0.0.1 right.wav)
+#  os.system (ffmpeg -i bird_sound -filter_complex stereotools=phasel=1 -ac 1 /home/jon//Documents/birdsong/output.wav)
 # ffmpeg -i XC2628_10Sec.wav -map_channel 0.0.0 OUTPUT_CH0 -map_channel 0.0.1 OUTPUT_CH1
+-filter_complex "[0:a]channelsplit=channel_layout=stereo[left][right]" -map "[left]" left.wav -map "[right]" right.wav
